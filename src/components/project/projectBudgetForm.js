8256b1e8f53cf3d21e0340a3ff87c7c2d7fa4f2e
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-11-06 09:03:41 -0600

    updates

--- src/components/project/projectBudgetForm.js
+++ src/components/project/projectBudgetForm.js
@@ -20,47 +20,51 @@ const ProjectBudgetForm = props => {
                 "Authorization": `Token ${localStorage.getItem("budgetapp_token")}`,
             }
         })
-        .then(res => res.json())
-        .then(setDept)
+            .then(res => res.json())
+            .then(setDept)
     }
 
     useEffect(getDepartments, [])
     const addProject = (e) => {
         e.preventDefault();
 
-        const new_proj = {
-            name: name.current.value,
-            length: parseInt(length.current.value),
-            dept: Object.values(project_dept),
-            // quantity: parseInt(quantity.current.value)
+        if (Object.keys(project_dept).length) {
+            const new_proj = {
+                name: name.current.value,
+                length: parseInt(length.current.value),
+                dept: Object.values(project_dept),
+                // quantity: parseInt(quantity.current.value)
+            }
+            submitProject(new_proj)
+        } else {
+            alert("please add a department")
         }
-        submitProject(new_proj)
     }
 
     const submitProject = (newproj) => {
-            return fetch("http://localhost:8000/projectbudgets", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": `Token ${localStorage.getItem("budgetapp_token")}`
-                },
-                body: JSON.stringify(newproj)
-            })
+        return fetch("http://localhost:8000/projectbudgets", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Token ${localStorage.getItem("budgetapp_token")}`
+            },
+            body: JSON.stringify(newproj)
+        })
             .then(() => {
                 props.history.push("/")
             })
-        }
+    }
 
     const toggleCheckbox = (dept) => {
-        if(document.getElementById(dept.id).checked === true){
-                setProjDept([...project_dept, dept])
-                // setId([...id, dept.id])
+        if (document.getElementById(dept.id).checked === true) {
+            setProjDept([...project_dept, dept])
+            // setId([...id, dept.id])
 
-            } else if(document.getElementById(dept.id).checked === false){
-                setProjDept(project_dept.filter(dep => dep !== dept))
-                // setId(id.filter(Id => Id !== dept.id))
-            }
+        } else if (document.getElementById(dept.id).checked === false) {
+            setProjDept(project_dept.filter(dep => dep !== dept))
+            // setId(id.filter(Id => Id !== dept.id))
+        }
 
     }
 
@@ -69,27 +73,27 @@ const ProjectBudgetForm = props => {
     return (
         <>
             <Header as="h1">Add A Project</Header>
-                <Form size="large" onSubmit={addProject}>
-                    <Label size="big" prompt basic>Project Name</Label>
-                        <input id="name" required defaultValue="" placeholder="Project Name" ref={name}>
-                        </input>
-                    <Label size="big" prompt basic>Project Length (months)</Label>
-                        <input id="length" required defaultValue="" placeholder="Project Length (in months)" type='number' ref={length}
-                        />
-            <Header as="h2">Choose Departments to Add to the Project</Header>
-                    {department.map((dept) =>
+            <Form size="large" onSubmit={addProject}>
+                <Label size="big" prompt basic>Project Name</Label>
+                <input id="name" required defaultValue="" placeholder="Project Name" ref={name}>
+                </input>
+                <Label size="big" prompt basic>Project Length (months)</Label>
+                <input id="length" required defaultValue="" placeholder="Project Length (in months)" type='number' ref={length}
+                />
+                <Header as="h2">Choose Departments to Add to the Project</Header>
+                {department.map((dept) =>
                     <div key={dept.id}>
                         <div>
                             <input
-                            id={dept.id}
-                            type="checkbox"
-                            value={dept.id}
-                            onClick={() => toggleCheckbox(dept)}/>{dept.name} - {dept.quantity} available
+                                id={dept.id}
+                                type="checkbox"
+                                value={dept.id}
+                                onClick={() => toggleCheckbox(dept)} />{dept.name} - {dept.quantity} available
                         </div>
                     </div>
-                    )}
+                )}
                 <Button color="blue" type="Submit">Add Project</Button>
-                </Form>
+            </Form>
 
 
         </>
